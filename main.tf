5da74cff3ee578df6ac75623c27a16babe409652
Sascha Gaspar <sascha.gaspar@gmx.de>
2023-06-28 08:53:06 +0200

    Fix script to create VM

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 # VMs
 resource "hcloud_server" "rancher_mgmt_nodes" {
-    count = var.server_count
-    name = "rancher-mgmt-${count.index + 1}"
-    image = "ubuntu-2204"
-    server_type = "cpx41"
+    name = "odroid-compile"
+    image = "ubuntu-22.04"
+    server_type = "cx11" # cpx41
     location = "nbg1"
     ssh_keys = [ "sgaspar@mbsgaspar", "sascha@router.homenet.saschagaspar.net" ]
 }
@@ -38,6 +37,7 @@ resource "terraform_data" "script" {
 
     provisioner "local-exec" {
         command = <<EOT
+            sleep 10
             ./setup.sh
             # ansible-playbook -u root -i inventory.yml ../Ansible/update.yaml
             ansible -i inventory.yml all -m ping
--- variables.tf
+++ variables.tf
@@ -2,9 +2,3 @@ variable "hcloud_token" {
     type = string
     description = "The token that is used to interact with the Hetzner Cloud API."
 }
-
-variable "server_count" {
-    type = number
-    default = 1
-    description = "Number of servers"
-}
